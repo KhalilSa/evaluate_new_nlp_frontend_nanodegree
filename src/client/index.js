dd97040a0abf6242d2c2c0b7bf310aa8ed82a8ee
Abraham <khalilsa01@gmail.com>
2021-03-26 16:24:32 +0300

    added

---
+++ src/client/index.js
@@ -0,0 +1,9 @@
+import { checkForName } from './js/nameChecker'
+import { handleSubmit, testPrint } from './js/formHandler'
+import img from './images/test.png'
+
+console.log(checkForName);
+testPrint('Hi');
+
+alert("I EXIST")
+console.log("CHANGE!!");
